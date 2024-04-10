5f3d7888ae56e16c5703e5f77ff4d77bffd6c12a
Vitaliy <mykhaylyuk66@gmail.com>
2024-04-10 09:30:13 +0200

    Minor changes
    - Make the 'update_coins_info' function perform batch saving of coinexchangeinfo
    - Update frontend build

--- api/cron.py
+++ api/cron.py
@@ -13,21 +13,31 @@ def update_coins_info():
             manager = ExchangeManagerFactory.create(e)
 
             tickers = manager.get_tickers_info()
+            tickers_info_list = []
             for ticker in tickers:
                 try:
                     if 'USDT' in manager.get_symbol(ticker):
                         formatted_t = manager.format_ticker_info(ticker)
-                        create_or_update_coin_exchange_info(formatted_t, e)
+
+                        info = {'ticker_info': formatted_t,
+                                'exchange_name': e,
+                                'info_date': date.today()}
+
+                        tickers_info_list.append(info)
                 except Exception as ex:
                     print(ex)
+
+            create_or_update_coin_exchange_info_bulk(tickers_info_list)
         except Exception as ex:
             print(ex)
 
 
 def create_or_update_coin_exchange_info(ticker_info, exchange_name, info_date=date.today()):
-    coin, c_created = Coin.objects.get_or_create(short_name=ticker_info['coin']) 
-    exchange, ex_created = Exchange.objects.get_or_create(slug=exchange_name, defaults={'name': exchange_name.capitalize()})
-    
+    coin, c_created = Coin.objects.get_or_create(
+        short_name=ticker_info['coin'])
+    exchange, ex_created = Exchange.objects.get_or_create(
+        slug=exchange_name, defaults={'name': exchange_name.capitalize()})
+
     coin_exchange_info, cx_created = CoinExchangeInfo.objects.update_or_create(
         coin=coin,
         exchange=exchange,
@@ -42,14 +52,17 @@ def create_or_update_coin_exchange_info(ticker_info, exchange_name, info_date=da
     )
 
 
-def create_or_update_coin_exchange_info_bulk(infos_list: List[Dict[str, any]]): # [ticker_info: dict, exchange_name: str, date: date]
+# [ticker_info: dict, exchange_name: str, date: date]
+def create_or_update_coin_exchange_info_bulk(infos_list: List[Dict[str, any]]):
     bulk_instances = []
-    
+
     for info in infos_list:
         ticker_info = info['ticker_info']
-        coin, c_created = Coin.objects.get_or_create(short_name=ticker_info['coin']) 
-        exchange, ex_created = Exchange.objects.get_or_create(slug=info['exchange_name'], defaults={'name': info['exchange_name'].capitalize()})
-        
+        coin, c_created = Coin.objects.get_or_create(
+            short_name=ticker_info['coin'])
+        exchange, ex_created = Exchange.objects.get_or_create(
+            slug=info['exchange_name'], defaults={'name': info['exchange_name'].capitalize()})
+
         coin_exchange_info = CoinExchangeInfo(
             coin=coin,
             exchange=exchange,
@@ -60,7 +73,7 @@ def create_or_update_coin_exchange_info_bulk(infos_list: List[Dict[str, any]]): 
             high_price=float(ticker_info['high_price']),
             low_price=float(ticker_info['low_price']),
         )
-        
+
         bulk_instances.append(coin_exchange_info)
-    
+
     CoinExchangeInfo.objects.bulk_create(bulk_instances, ignore_conflicts=True)
