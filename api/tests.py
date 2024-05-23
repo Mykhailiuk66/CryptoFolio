34cae1a364f78ba7f60c8e5521c023629624bb96
Vitalii <110096259+Mykhailiuk66@users.noreply.github.com>
2024-05-23 17:29:01 +0200

    Add test example

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from api.models import Coin
 
-# Create your tests here.
+class CoinModelAPITest(APITestCase):
+    def setUp(self):
+        Coin.objects.create(short_name='BTC', name='Bitcoin')
+
+    def test_get_my_model_list(self):
+        url = '/api/coins/'
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
